2e60a4ec67d6b22b054b69b28bbdc5e436480406
Burlistin <ilia005.ilia005@gmail.com>
2026-01-20 14:52:44 +0100

    Compartmentalized the algorithm to work separately for the 19th and 21st century (virtually extendable for any)

--- filter.py
+++ filter.py
@@ -10,9 +10,9 @@ for letter in alphabet :
         data.append(json.load(file))
 
 # initialize variables
-deathplace = []
-deathcount = {}
-final_deathcount = Counter()
+deathplace = {'19': [], '21': []}
+deathcount = {'19': {}, '21': {}}
+final_deathcount = {'19': Counter(), '21': Counter()}
 
 # filter for only entries that include death place
 for person in data:
@@ -23,31 +23,35 @@ for person in data:
                     year = int(info['ontology/deathDate'].split('-')[0])
                 except ValueError:
                     year = 0
+                if year >= 1800 and year < 1900:
+                    deathplace['19'].append(info['ontology/deathPlace_label'])
                 if year >= 2000:
-                    deathplace.append(info['ontology/deathPlace_label'])
+                    deathplace['21'].append(info['ontology/deathPlace_label'])
 
 # counting number of deaths in each place
-for places in deathplace:
-    # if it's a list
-    if isinstance(places, list): 
-        for place in places:
-            place = place.lower()
-            if place in deathcount:
-                deathcount[place] += 1
+for century in ['19', '21']:
+    for places in deathplace[century]:
+        # if it's a list
+        if isinstance(places, list): 
+            for place in places:
+                place = place.lower()
+                if place in deathcount[century]:
+                    deathcount[century][place] += 1
+                    # filter for values above 10
+                    if deathcount[century][place] >= 10:
+                        final_deathcount[century][place] = deathcount[century][place]
+                else:
+                    deathcount[century][place] = 1
+        # if it's a string
+        else:                       
+            places = places.lower()
+            if places in deathcount[century]:   
+                deathcount[century][places] += 1
                 # filter for values above 10
-                if deathcount[place] >= 10:
-                    final_deathcount[place] = deathcount[place]
+                if deathcount[century][places] >= 10:
+                    final_deathcount[century][places] = deathcount[century][places]
             else:
-                deathcount[place] = 1
-    # if it's a string
-    else:                       
-        places = places.lower()
-        if places in deathcount:   
-            deathcount[places] += 1
-            # filter for values above 10
-            if deathcount[places] >= 10:
-                final_deathcount[places] = deathcount[places]
-        else:
-            deathcount[places] = 1
+                deathcount[century][places] = 1
 
-print(final_deathcount)
+print(final_deathcount['19'])
+print(final_deathcount['21'])
