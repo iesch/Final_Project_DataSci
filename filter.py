0a7e0c196646e6af828ea03b2c00ba66a194c8ee
Eduardo <ehallabreu@gmail.com>
2026-01-19 17:24:29 -0800

    best cleanest code for counting deaths

---
+++ filter.py
@@ -0,0 +1,34 @@
+import json
+
+# load the data
+with open('People/A_people.json') as file:
+    data = json.load(file)
+
+deathplace = []
+for row in data: 
+    if 'ontology/deathPlace_label' in row:
+        deathplace.append(row['ontology/deathPlace_label'])
+
+deathcount = {}
+final_deathcount = {}
+
+for places in deathplace:
+    if isinstance(places, list):
+        for place in places:
+            if place in deathcount:
+                deathcount[place] += 1
+                if deathcount[place] >= 10:
+                    final_deathcount[place] = deathcount[place]
+            else:
+                deathcount[place] = 1
+    else:
+        if places in deathcount:
+            deathcount[places] += 1
+            if deathcount[places] >= 10:
+                    final_deathcount[places] = deathcount[places]
+        else:
+            deathcount[places] = 1
+
+
+
+print(final_deathcount)
