b76e87b8c7270fd4ca26de7fd323cf28a383832d
Igone Schotman <i.e.schotman@students.uu.nl>
2026-01-21 16:00:43 +0100

    MAP!

--- ggplot_mapping.R
+++ ggplot_mapping.R
@@ -4,28 +4,20 @@ library(maps)
 
 coordinates <- map_data("world")
 
-ggplot() +
-  geom_map(
-    data = coordinates, map = coordinates,
-    aes(long, lat, map_id = region),
-    color = "black", fill = "lightgreen", linewidth = 0.2 )+
-  theme(panel.background = element_rect(fill = "lightblue"),
-      panel.grid.minor = element_line(color="lightblue"),
-      panel.grid.major= element_line(color="lightblue"),
-      axis.ticks.y = element_blank(),
-      axis.ticks.x = element_blank()) +
-  scale_x_continuous(labels = NULL) +
-  scale_y_continuous(labels = NULL) +
-  labs(x = NULL, y = NULL)
- 
 data_19 <- read_csv('Results/deathcount_19.csv')
 data_20 <- read_csv('Results/deathcount_21.csv')
 
-data_mapping <- left_join(data_19, coordinates, by = 'region')
-ggplot(data = data_mapping, aes(x='long', y ='lat', fill = 'region'))+
-  geom_polygon(aes(fill= 'deathcount'))+
-  labs(fill= 'deathcount') +
-  scale_fill_discrete()
+data_mapping <- right_join(data_20, coordinates, by = 'region') |>
+  mutate(deathcount = replace_na(deathcount, 0))
+
+ggplot(data = data_mapping) +
+  aes(x=long, y =lat, fill = deathcount, map_id = region) +
+  geom_map(map = data_mapping) + 
+  scale_x_continuous(labels = NULL) +
+  scale_y_continuous(labels = NULL) +
+  labs(x = NULL, y = NULL) +
+  scale_fill_continuous(low = 'pink', high = 'red') +
+coord_fixed(1.3)
 
 
 
